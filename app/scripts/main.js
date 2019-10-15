28e43ffa297ce9f9dea9ad59ca78879c6577e4ed
Mathitz <matheusalves789@gmail.com>
2019-10-15 17:11:09 -0300

    Fixing images size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+// MENU
+
 const menuIcon = document.querySelector('.header-nav__icon');
 
 const menuOpen = document.querySelector('.navigation-section');
@@ -11,3 +13,23 @@ menuIcon.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
   menuOpen.classList.remove('navigation__menu');
 });
+
+// MENU
+
+// MENU CIRCLE
+
+const items = document.querySelectorAll('.navigation-menu__items');
+
+let menuItems = Array.from(items[0].children);
+
+menuItems.forEach(i => {
+  i.addEventListener('click', () => {
+    if (i.classList.contains('navigation-menu__item--checked')) {
+      i.classList.remove('navigation-menu__item--checked');
+    } else {
+      i.classList.add('navigation-menu__item--checked');
+    }
+  });
+});
+
+// MENU CIRCLE
